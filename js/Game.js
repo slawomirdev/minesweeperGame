3d096610c3c23e897d40064140cb3ffb23e9b3f5
thewilkupl <thewilku@o2.pl>
2022-08-06 23:59:50 +0200

    initialize Game

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,55 @@
-console.log("Hello game")
+import { Cell } from './Cell.js'
+
+class Game {
+    #config = {
+        easy: {
+            rows: 8,
+            cols: 8,
+            mines: 10
+        },
+        medium: {
+            rows: 16,
+            cols: 16,
+            mines: 40
+        },
+        expert: {
+            rows: 16,
+            cols: 30,
+            mines: 99
+        }
+    }
+
+    #numberOfRows = null;
+    #numberOfCols = null;
+    #numberOfMines = null;
+    #cells = [];
+
+    constructor() {
+    }
+
+    initializeGame(){
+        this.#newGame();
+    }
+
+    #newGame(rows = this.#config.easy.rows, cols = this.#config.easy.cols, mines = this.#config.easy.mines){
+        this.#numberOfRows = rows;
+        this.#numberOfCols = cols;
+        this.#numberOfMines = mines;
+
+        this.#generateCells();
+    }
+
+    #generateCells(){
+        for(let row = 0; row < this.#numberOfRows; row++){
+            this.#cells[row] = []
+            for(let col = 0; col < this.#numberOfCols; col++){
+                this.#cells[row].push(new Cell(col, row));
+            }
+        }
+    }
+}
+
+window.onload = function () {
+    const game = new Game();
+    game.initializeGame()
+}

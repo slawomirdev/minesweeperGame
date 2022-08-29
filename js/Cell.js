e5eda05c3a6a4227415b98fe150a5f76aa5585ea
thewilkupl <thewilku@o2.pl>
2022-08-29 22:48:05 +0200

    cell mine numbers

--- js/Cell.js
+++ js/Cell.js
@@ -25,6 +25,11 @@ export class Cell extends UI {
 
         if(this.isMine) {
             this.element.classList.add('cell--is-mine');
+            return;
+        }
+        if(this.value) {
+            this.element.textContent = this.value;
+            this.element.classList.add(`cell-info-${this.value}`)
         }
     }
 
